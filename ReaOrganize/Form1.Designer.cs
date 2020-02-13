a9a6eccea0fb6a5c17ccda37e61e55c0f43776bd
James Parsons <jparsons68@yahoo.com.au>
2020-02-13 11:28:10 +1100

    Add project files.

---
+++ ReaOrganize/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ReaOrganize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileFieldAndBrowser1 = new IERSInterface.FileFieldAndBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // fileFieldAndBrowser1
+            // 
+            this.fileFieldAndBrowser1.AllowDrop = true;
+            this.fileFieldAndBrowser1.BrowseFoldersOnly = true;
+            this.fileFieldAndBrowser1.CheckFileExists = true;
+            this.fileFieldAndBrowser1.FileName = "";
+            this.fileFieldAndBrowser1.Filter = "All Files|*.*";
+            this.fileFieldAndBrowser1.FilterIndex = 1;
+            this.fileFieldAndBrowser1.Location = new System.Drawing.Point(9, 9);
+            this.fileFieldAndBrowser1.Margin = new System.Windows.Forms.Padding(0);
+            this.fileFieldAndBrowser1.Multiselect = false;
+            this.fileFieldAndBrowser1.Name = "fileFieldAndBrowser1";
+            this.fileFieldAndBrowser1.RecentFileRegistry = null;
+            this.fileFieldAndBrowser1.Size = new System.Drawing.Size(512, 25);
+            this.fileFieldAndBrowser1.TabIndex = 0;
+            this.fileFieldAndBrowser1.TextOffset = 94;
+            this.fileFieldAndBrowser1.TextWidth = 343;
+            this.fileFieldAndBrowser1.Title = null;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(499, 272);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 350);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.fileFieldAndBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private IERSInterface.FileFieldAndBrowser fileFieldAndBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ReaOrganize/Form1.cs
@@ -0,0 +1,117 @@
+﻿using QSFileReader;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ReaOrganize
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean didSomething = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+
+            string[] exts = new string[] { "*.wav" };
+
+
+
+            // find something NOT in RR directory
+
+            PathProcessor pathProc = new PathProcessor(
+                            delegate(string path, Boolean load)
+                            {
+                                try
+                                {
+                                    string conatiningDir = Path.GetDirectoryName(path);
+                                    // is in RR?
+                                    Boolean isRR = false;
+                                    for (int i = 1; i <= 9; i++)
+                                    {
+                                        if (conatiningDir.EndsWith("\\RR" + i)) { isRR = true; break; }
+                                    }
+                                    if (isRR) return (true);
+
+                                    // no
+                                    _processFile(path);
+                                    didSomething = true;
+                                    return (false);//stop?
+                                }
+                                catch
+                                {
+                                }
+
+                                return (false);
+                            }, exts);
+
+            didSomething = true;
+            while (didSomething)
+            {
+                didSomething = false;
+                pathProc.Process(fileFieldAndBrowser1.FileName, true);
+            }
+
+
+            string filename = fileFieldAndBrowser1.FileName;
+            textBox1.AppendText(filename);
+            textBox1.AppendText(Environment.NewLine);
+
+        }
+
+
+
+        void _processFile(string filename)
+        {
+            // get the folder the file is in
+            string path = System.IO.Path.GetDirectoryName(filename);
+            // do per code
+            moveToSub(path, "CL_HT", "Close");
+            moveToSub(path, "RM_HT", "Room");
+            moveToSub(path, "OH_HT", "Overhead");
+        }
+
+
+
+        private void moveToSub(string path, string p, string groupSub)
+        {
+            textBox1.AppendText("CODE:" + p);
+            textBox1.AppendText(Environment.NewLine);
+
+            DirectoryInfo di = null;
+
+            textBox1.AppendText(Environment.NewLine);
+            // get files that match code
+            for (int i = 1; i < 100; i++)
+            {
+                string rrFilt = "*" + p + "*_RR" + i + "*.wav";
+                string[] fileList = System.IO.Directory.GetFiles(path, rrFilt);
+                if (fileList == null || fileList.Length <= 0) break;
+
+                // make group sub
+                string sub = System.IO.Path.Combine(path, groupSub);
+                textBox1.AppendText("DIR:" + sub);
+                if (!System.IO.Directory.Exists(sub))
+                    di = System.IO.Directory.CreateDirectory(System.IO.Path.Combine(path, groupSub));
+
+                DirectoryInfo rrDI = Directory.CreateDirectory(System.IO.Path.Combine(sub, "RR" + i.ToString()));
+
+                foreach (var item in fileList)
+                {
+                    System.IO.File.Move(item, System.IO.Path.Combine(rrDI.FullName, Path.GetFileName(item)));
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+}
